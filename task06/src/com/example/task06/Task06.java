1197d453e1394e3c2f97916162876bdf1263414f
Fisottis <ejs.defolt@gmail.com>
2019-09-22 00:32:36 +0500

    Гаврилов Егор task 06

--- task06/src/com/example/task06/Task06.java
+++ task06/src/com/example/task06/Task06.java
@@ -5,17 +5,23 @@ public class Task06 {
     public static int solution(int x, int y) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        int summ, counter;
+        summ = x + y;
+        String str = Integer.toString(summ);
+        if (summ > 0) counter = str.length();
+        else if (summ == 0) counter = 1;
+        else counter = str.length() - 1;
 
-        return 0;
+        return counter;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(12, 34);
+
+        int result = solution(634234, 78623423);
         System.out.println(result);
-        */
+
     }
 
 }
