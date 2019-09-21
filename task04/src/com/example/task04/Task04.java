6abfc78d864754281c7ff4156096d64600a3a6d4
Fisottis <ejs.defolt@gmail.com>
2019-09-22 00:31:56 +0500

    Гаврилов Егор task 04

--- task04/src/com/example/task04/Task04.java
+++ task04/src/com/example/task04/Task04.java
@@ -5,17 +5,27 @@ public class Task04 {
     public static float calculate(int a, int b, String operation) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        float result, firstNumber, secondNumber;
+        firstNumber = (float)(a);
+        secondNumber = (float)(b);
 
-        return 0;
+        if (operation == "+")  result = firstNumber + secondNumber;
+
+        else if (operation == "*")  result = firstNumber * secondNumber;
+
+        else if (operation == "/")  result = firstNumber / secondNumber;
+
+        else  result = firstNumber - secondNumber;
+        return result;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         float result = calculate(-25, 5, "/");
         System.out.println(result);
-        */
+
     }
 
 }
