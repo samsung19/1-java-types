c95bf6efb66276872eb7c916557f47bcff99a7a8
Fisottis <ejs.defolt@gmail.com>
2019-09-22 00:33:28 +0500

    Гаврилов Егор task 14

--- task14/src/com/example/task14/Task14.java
+++ task14/src/com/example/task14/Task14.java
@@ -1,22 +1,29 @@
 package com.example.task14;
 
+
+
 public class Task14 {
 
 
     public static int reverse(int value) {
 
         // TODO напишите здесь код, переставляющий цифры числа в обратном порядке
-
-        return 0;
+        String word = Integer.toString(value);
+        StringBuilder str = new StringBuilder(word);
+        StringBuilder revStr = str.reverse();
+        word = revStr.toString();
+        value = Integer.parseInt(word);
+        return value;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
         int result = reverse(345);
         System.out.println(result);
-         */
+
+
+
     }
 
 
