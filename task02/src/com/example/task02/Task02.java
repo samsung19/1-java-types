f83723b8c40b3e51e7e0c32e0f6562dd92ad8c60
Fisottis <ejs.defolt@gmail.com>
2019-09-22 00:31:18 +0500

    Гаврилов Егор task 02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -5,17 +5,24 @@ public class Task02 {
     public static String solution(String input) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        String result;
+        long variable = Long.parseLong(input);
+        if (variable >= -128 && variable <= 127) result = "byte";
 
-        return "";
+        else if (variable >= -32768 && variable <= 32767) result = "short";
+
+        else if (variable >= -2147483648 && variable <= 2147483647) result = "int";
+
+        else result = "long";
+        return result;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution("12345");
+        String result = solution("0");
         System.out.println(result);
-         */
+
     }
 
 }
