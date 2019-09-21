97c2426a0bd84758114b6151577f4aab88e4c778
Fisottis <ejs.defolt@gmail.com>
2019-09-22 00:31:38 +0500

    Гаврилов Егор task 03

--- task03/src/com/example/task03/Task03.java
+++ task03/src/com/example/task03/Task03.java
@@ -5,17 +5,18 @@ public class Task03 {
     public static int getMetreFromCentimetre(int centimetre) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return 0;
+        int meters = centimetre / 100;
+        int result = meters;
+        return result;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        int result = solution(345);
+
+        int result = getMetreFromCentimetre(345);
         System.out.println(result);
-         */
+
     }
 
 }
