6af12583b7513c1933deb66db9783bfe202727c7
Fisottis <ejs.defolt@gmail.com>
2019-09-22 00:32:19 +0500

    Гаврилов Егор task 05

--- task05/src/com/example/task05/Task05.java
+++ task05/src/com/example/task05/Task05.java
@@ -5,17 +5,18 @@ public class Task05 {
     public static String solution(int x) {
 
         // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
+        String message = "FALSE";
+        if ((x%10)%2==0 && (x/10%10) % 2==0 && (x / 100 % 10) % 2 ==0 && (x / 1000 % 10) % 2 ==0) message = "TRUE";
 
-        return "FALSE";
+        return message;
     }
 
     public static void main(String[] args) {
         // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
-        String result = solution(1234);
+
+        String result = solution(1);
         System.out.println(result);
-        */
     }
 
 }
