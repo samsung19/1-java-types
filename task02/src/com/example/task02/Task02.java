431f7dbd465983301bc7030a3e956f7072ae1c57
Leokrut5 <55390426+Leokrut5@users.noreply.github.com>
2019-09-22 19:33:24 +0500

    Tkalich Leonid task1

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -1,21 +1,20 @@
 package com.example.task02;
 
+
 public class Task02 {
 
     public static String solution(String input) {
-
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
+        long n = Long.parseLong(input);
+        if ((n <= 127) && (n>=-128)) {
+            return  "byte";
+        } else if ((n <= 32767) && (n>=-32768)) {
+            return ("short");
+        } else if ((n>= -2147483648)&&(n<=2147483647)) {
+            return ("int");
+        } else {
+            return ( "long");
+        }
     }
-
-    public static void main(String[] args) {
-        // Здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-        /*
-        String result = solution("12345");
-        System.out.println(result);
-         */
-    }
-
 }
+
+
