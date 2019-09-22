23b8eea5cac1266e834ba382ee7f9ee18849d59d
milesha <mi.serbinova@gmail.com>
2019-09-22 11:56:35 +0500

    Serbinova Milena task02

--- task02/src/com/example/task02/Task02.java
+++ task02/src/com/example/task02/Task02.java
@@ -3,10 +3,15 @@ package com.example.task02;
 public class Task02 {
 
     public static String solution(String input) {
+        long input_copy = Long.parseLong(input);
+        if (input_copy >= -128 & input_copy <= 127)
+            return "byte";
+        else if(input_copy >= -32768 & input_copy <= 32767)
+            return "short";
+        else if(input_copy >= -2147483648 & input_copy <= 2147483647)
+            return "int";
+        else return "long";
 
-        // TODO напишите здесь свою корректную реализацию этого метода, вместо сеществующей
-
-        return "";
     }
 
     public static void main(String[] args) {
